7e3fb5e899ab84664841b371a0ce02aea0542922
Lucas Braga <130006732+LucasBraga2@users.noreply.github.com>
2023-09-14 21:55:13 -0300

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -4,13 +4,18 @@
 void cria_jogador(jogador *j){
 
     printf("\nInsira o nome do jogador:\n");
-    scanf("%[^\n]s", &j->nome);
+    scanf(" %[^\n]s", &j->nome);
     j->h = 20;
     j->e = 5;
 
     printf("#####################################\n");
     printf("Nome: %s \nVida: %d Hp \nEnergia: %d/5 \n", j->nome, j->h, j->e);
     printf("#####################################\n");
+
+}
+
+void cria_cartas(){
+
 
 }
 
@@ -25,6 +30,36 @@ void mostrar_cartas(){
     int t;//TIPOS DE CARTAS
     char t2 = 'N';
 
+carta c1;//CARTA 1
+
+
+carta c2;//CARTA 2
+
+
+carta c3;//CARTA 3
+
+
+carta c4;//CARTA 4
+
+
+carta c5;//CARTA 5
+
+
+carta c6;//CARTA 6
+
+
+carta c7;//CARTA 7
+
+carta c8;//CARTA 8
+
+
+carta c9;//CARTA 9
+
+
+carta c10;//CARTA 10
+
+
+
 while(t2 == 'N' || t2 =='n'){
     printf("Digite 1 para Ataques\n");
     printf("Digite 2 para Defesas\n");
@@ -34,55 +69,55 @@ while(t2 == 'N' || t2 =='n'){
 
     if(t == 1){
         printf("#####################################\n");//CARTA 1
-        printf("#             Nome: Chute           #\n");                    
-        printf("Tipo:           Ataque              #\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Nome: Chute\n");                    
+        printf("Tipo: Ataque\n");
+        printf("Valor: %d\n", c1.v);
+        printf("Custo: %d energia\n", c1.c);
         printf("#####################################\n");//CARTA 2
         printf("Nome: Soco\n");
         printf("Tipo: Ataque\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Valor:%d\n", c2.v);
+        printf("Custo: %d energia\n", c2.c);
         printf("#####################################\n");//CARTA 3
         printf("Nome: Facada\n");
         printf("Tipo: Ataque\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Valor: %d\n", c3.v);
+        printf("Custo: %d energia\n", c3.c);
         printf("#####################################\n");//CARTA 4
         printf("Nome: Espadada\n");
         printf("Tipo: Ataque\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Valor: %d\n", c4.v);
+        printf("Custo: %d energia\n", c4.c);
         printf("#####################################\n");//CARTA 5
         printf("Nome: Facadas Duplas\n");
         printf("Tipo: Ataque\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Valor: %d\n", c5.v);
+        printf("Custo: %d energia\n", c5.c);
         printf("#####################################\n");//CARTA 6
         printf("Nome: Martelo de Espinhos\n");
         printf("Tipo: Ataque\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Valor: %d\n", c6.v);
+        printf("Custo: %d energia\n", c6.c);
         printf("#####################################\n");//CARTA 7
         printf("Nome: Lanca-chamas\n");
         printf("Tipo: Ataque\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Valor: %d\n", c7.v);
+        printf("Custo: %d energia\n", c7.c);
         printf("#####################################\n");//CARTA 8
         printf("Nome: Bombas Explosivas\n");
         printf("Tipo: Ataque\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Valor: %d\n", c8.v);
+        printf("Custo: %d energia\n", c8.c);
         printf("#####################################\n");//CARTA 9
         printf("Nome: Chuva de Asteroidess\n");
         printf("Tipo: Ataque\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Valor: %d\n", c9.v);
+        printf("Custo: %d energia\n", c9.c);
         printf("#####################################\n");//CARTA 10
         printf("Nome: Raios Infinitos\n");
         printf("Tipo: Ataque\n");
-        printf("Valor: 5\n");
-        printf("Custo: 1 energia\n");
+        printf("Valor: %d\n", c10.v);
+        printf("Custo: %d energia\n", c10.c);
         printf("#####################################\n");
     }
     
@@ -226,7 +261,7 @@ int main(){
     tp_pilha p_deck;//Deck de Cartas do jogo
     char escolha;
     
-    //cria_cartas();
+    cria_cartas();
 
     printf("Deseja visualizar as cartas disponiveis no jogo:\n");
     printf("S/N\n");
