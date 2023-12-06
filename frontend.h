42dbab17e657ea213f0ec1fedf0fb24c3ee19ca9
filipe <filipelorenzoarraes@gmail.com>
2023-12-06 11:23:09 -0300

    cobra de duas cbç frontend

--- frontend.h
+++ frontend.h
@@ -1,82 +1,6 @@
 #include <stdio.h>
 #include "monstro.h"
 
-void player_e_monstro(jogador *j, monstro *m, int mns)
-{
-
-    printf("\n");
-    printf("        __                             ________       \n");
-    printf("       |__|                             |____|     \n");
-    printf("      _ || _                              ||        \n");
-    printf("     | |--| |                          |-|--|-|     \n");
-    printf("     | |  | |                          | |  | |     \n");
-    printf("       |__|                            __|__|__     \n");
-    printf("       |  |                           /  |  |  |    \n");
-    printf("       |_ |_                         _| _| _| _|    \n\n");
-    printf("    Player: %s                     Monstro: %s     \n", j->nome, m[mns].nome);
-    printf(" Vida: %d/20   Energia: %d/5         Vida do Monstro: %d/20\n\n", j->h, j->e, m[mns].h);
-    printf("         Escudo: %d                            \n\n", j->esc);
-}
-
-void monstro_ataca_player()
-{
-    printf("\n");
-    printf("        __                              ________     \n");
-    printf("       |__|            _____             |____|     \n");
-    printf("      _ || _       <|-|/////|              ||        \n");
-    printf("     | |--| |      <|-|/////|           |-|--|-|     \n");
-    printf("     | |  | |      <|-|/////|           | |  | |     \n");
-    printf("       |__|        <|-|_____|           __|__|__     \n");
-    printf("       |  |                           /  |  |  |    \n");
-    printf("       |_ |_                         _| _| _| _|    \n\n");
-    printf("    Player: Eren                  Monstro: Homem Gosma     \n");
-    printf(" Vida: 20   Energia: 5            Vida do Monstro: 40     \n\n");
-}
-
-void player_se_defende()
-{
-    printf("\n");
-    printf("        __         ___                  ________      \n");
-    printf("       |__|      ||///||                 |____|     \n");
-    printf("      _ || _     ||///||                   ||        \n");
-    printf("     | |--| |  |-||///||                |-|--|-|     \n");
-    printf("     | |  | |--| ||///||                | |  | |     \n");
-    printf("       |__|    |-||///||                __|__|__     \n");
-    printf("       |  |      ||///||               /  |  |  |    \n");
-    printf("       |_ |_     ||___||              _| _| _| _|    \n\n");
-    printf("    Player: Eren                   Monstro: Homem Gosma     \n");
-    printf(" Vida: 20   Energia: 5             Vida do Monstro: 40     \n\n");
-}
-
-void mosntro_se_defende()
-{
-    printf("\n");
-    printf("        __                    ___       ________     \n");
-    printf("       |__|                 ||///||      |____|      \n");
-    printf("      _ || _                ||///||        ||        \n");
-    printf("     | |--| |               ||///||-|   |-|--|-|     \n");
-    printf("     | |  | |               ||///|| |---| |  | |     \n");
-    printf("       |__|                 ||///||-|   __|__|__     \n");
-    printf("       |  |                 ||///||    /  |  |  |    \n");
-    printf("       |_ |_                ||___||   _| _| _| _|    \n\n");
-    printf("    Player: Eren                  Monstro: Homem Gosma     \n");
-    printf(" Vida: 20   Energia: 5            Vida do Monstro: 40     \n\n");
-}
-
-void player_ataca_monstro(){
-
-    printf("\n");
-    printf("        __                             ________       \n");
-    printf("       |__|           _____             |____|     \n");
-    printf("      _ || _         |/////|-|>           ||        \n");
-    printf("     | |--| |        |/////|-|>        |-|--|-|     \n");
-    printf("     | |  | |        |/////|-|>        | |  | |     \n");
-    printf("       |__|          |_____|-|>        __|__|__     \n");
-    printf("       |  |                           /  |  |  |    \n");
-    printf("       |_ |_                         _| _| _| _|    \n\n");
-    printf("    Player: Eren                  Monstro: Homem Gosma     \n");
-    printf(" Vida: 20   Energia: 5            Vida do Monstro: 40     \n\n");
-}
 void player_ganha(jogador *j)
 {
     printf("\n");
@@ -108,3 +32,122 @@ void player_morre(jogador *j)
     printf("       Vida: 0/20         \n\n");
     printf("      VOCE MORREU!            \n\n");
 }
+
+
+void player_e_monstro(jogador *j, monstro *m, int mns)
+{
+
+    printf("\n");
+    printf("        __                             ________       \n");
+    printf("       |__|                             |____|     \n");
+    printf("      _ || _                              ||        \n");
+    printf("     | |--| |                          |-|--|-|     \n");
+    printf("     | |  | |                          | |  | |     \n");
+    printf("       |__|                            __|__|__     \n");
+    printf("       |  |                           /  |  |  |    \n");
+    printf("       |_ |_                         _| _| _| _|    \n\n");
+    printf("    Player: %s                     Monstro: %s     \n", j->nome, m[mns].nome);
+    printf(" Vida: %d/20   Energia: %d/5         Vida do Monstro: %d/20\n\n", j->h, j->e, m[mns].h);
+    printf("         Escudo: %d                            \n\n", j->esc);
+}
+
+/*void player_e_monstro4(jogador *j, monstro *m, int mns)
+{
+
+    printf("\n");
+    printf("        __                           _       _                           \n");
+    printf("       |__|                         |_|     |_|                          \n");
+    printf("      _ || _                         ||     ||                           \n");
+    printf("     | |--| |                         \\\\   //                          \n");
+    printf("     | |  | |                          \\\\ //                 _         \n");
+    printf("       |__|                          \\   \\\\     /          /   \\     \n");
+    printf("       |  |                           \\---\\\\---/           |_/  |     \n");
+    printf("       |_ |_                               \\\\__________________/     \n\n");
+    printf("    Player: %s                          Monstro: %s     \n", j->nome, m[mns].nome);
+    printf(" Vida: %d/20   Energia: %d/5               Vida do Monstro: %d/20\n\n", j->h, j->e, m[mns].h);
+    printf("         Escudo: %d                             \n\n", j->esc);
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*void monstro_ataca_player()
+{
+    printf("\n");
+    printf("        __                              ________     \n");
+    printf("       |__|            _____             |____|     \n");
+    printf("      _ || _       <|-|/////|              ||        \n");
+    printf("     | |--| |      <|-|/////|           |-|--|-|     \n");
+    printf("     | |  | |      <|-|/////|           | |  | |     \n");
+    printf("       |__|        <|-|_____|           __|__|__     \n");
+    printf("       |  |                           /  |  |  |    \n");
+    printf("       |_ |_                         _| _| _| _|    \n\n");
+    printf("    Player: Eren                  Monstro: Homem Gosma     \n");
+    printf(" Vida: 20   Energia: 5            Vida do Monstro: 40     \n\n");
+}*/
+
+/*void player_se_defende()
+{
+    printf("\n");
+    printf("        __         ___                  ________      \n");
+    printf("       |__|      ||///||                 |____|     \n");
+    printf("      _ || _     ||///||                   ||        \n");
+    printf("     | |--| |  |-||///||                |-|--|-|     \n");
+    printf("     | |  | |--| ||///||                | |  | |     \n");
+    printf("       |__|    |-||///||                __|__|__     \n");
+    printf("       |  |      ||///||               /  |  |  |    \n");
+    printf("       |_ |_     ||___||              _| _| _| _|    \n\n");
+    printf("    Player: Eren                   Monstro: Homem Gosma     \n");
+    printf(" Vida: 20   Energia: 5             Vida do Monstro: 40     \n\n");
+}*/
+
+/*void mosntro_se_defende()
+{
+    printf("\n");
+    printf("        __                    ___       ________     \n");
+    printf("       |__|                 ||///||      |____|      \n");
+    printf("      _ || _                ||///||        ||        \n");
+    printf("     | |--| |               ||///||-|   |-|--|-|     \n");
+    printf("     | |  | |               ||///|| |---| |  | |     \n");
+    printf("       |__|                 ||///||-|   __|__|__     \n");
+    printf("       |  |                 ||///||    /  |  |  |    \n");
+    printf("       |_ |_                ||___||   _| _| _| _|    \n\n");
+    printf("    Player: Eren                  Monstro: Homem Gosma     \n");
+    printf(" Vida: 20   Energia: 5            Vida do Monstro: 40     \n\n");
+}*/
+
+/*void player_ataca_monstro(){
+
+    printf("\n");
+    printf("        __                             ________       \n");
+    printf("       |__|           _____             |____|     \n");
+    printf("      _ || _         |/////|-|>           ||        \n");
+    printf("     | |--| |        |/////|-|>        |-|--|-|     \n");
+    printf("     | |  | |        |/////|-|>        | |  | |     \n");
+    printf("       |__|          |_____|-|>        __|__|__     \n");
+    printf("       |  |                           /  |  |  |    \n");
+    printf("       |_ |_                         _| _| _| _|    \n\n");
+    printf("    Player: Eren                  Monstro: Homem Gosma     \n");
+    printf(" Vida: 20   Energia: 5            Vida do Monstro: 40     \n\n");
+}*/
--- rascunho.c
+++ rascunho.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("\n");
-    printf("                               \n");
-    printf("                               \n");
-    printf("         _    _                \n");
-    printf("        | |--| |               \n");
-    printf("        | |  | |               \n");
-    printf("          |__|                 \n");
-    printf("          |  |       __        \n");
-    printf("         _|  |_     |__|       \n\n");
-    printf("      Player: Eren             \n");
-    printf("   Vida: 20   Energia: 5      \n\n");
-    printf("      VOCE MORREU!            \n\n");
+   printf("\n");
+    printf("        __                           _       _                           \n");
+    printf("       |__|                         |_|     |_|                          \n");
+    printf("      _ || _                         ||     ||                           \n");
+    printf("     | |--| |                         \\\\   //                          \n");
+    printf("     | |  | |                          \\\\ //                 _         \n");
+    printf("       |__|                          \\   \\\\     /          /   \\     \n");
+    printf("       |  |                           \\---\\\\---/           |_/  |     \n");
+    printf("       |_ |_                               \\\\__________________/     \n\n");
+    printf("    Player: vadvsav                   Monstro: Cobra de Duas Cabecas      \n");
+    printf(" Vida: 20/20   Energia: 5/5               Vida do Monstro: 30\n");
+    printf("         Escudo: 0                             \n\n");
 }
